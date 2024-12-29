85a0ef2b437a712577b658e17dd5566a2b429c93
GITMOHI <cmohi121@gmail.com>
2024-12-29 22:27:36 +0600

    serverless call added

--- api/news.js
+++ api/news.js
@@ -1,25 +1,19 @@
 import fetch from 'node-fetch';
 
-module.exports = async (req, res) => {
-  const apiKey = 'b8504bb386304c0b8cc1161f3a8cc2d8'; // Replace with your NewsAPI key
+export default async function handler(req, res) {
+  const apiKey = 'b8504bb386304c0b8cc1161f3a8cc2d8';
   const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
 
   try {
-    console.log('Fetching news from NewsAPI...');
     const response = await fetch(apiUrl);
-    console.log('NewsAPI Response Status:', response.status);
-
     const data = await response.json();
-    console.log('NewsAPI Response Data:', data);
 
     if (response.ok) {
       res.status(200).json(data);
     } else {
-      console.error('NewsAPI Error:', data);
       res.status(response.status).json({ error: data.message });
     }
   } catch (error) {
-    console.error('Serverless Function Error:', error.message);
     res.status(500).json({ error: 'Internal Server Error' });
   }
-};
+}
